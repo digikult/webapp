304bf5bf3be47f1112abaa2f0ede51521b7ad40e
Micke Nordin <mik@elnord.in>
2013-04-10 23:33:11 +0200

    Now with clickable map

--- js/maps.js
+++ js/maps.js
@@ -8,4 +8,36 @@ function map_init() {
  
 	map.addLayer(mapnik);
 	map.setCenter(position, zoom );
+	OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
+                defaultHandlerOptions: {
+                    'single': true,
+                    'double': false,
+                    'pixelTolerance': 0,
+                    'stopSingle': false,
+                    'stopDouble': false
+                },
+
+                initialize: function(options) {
+                    this.handlerOptions = OpenLayers.Util.extend(
+                        {}, this.defaultHandlerOptions
+                    );
+                    OpenLayers.Control.prototype.initialize.apply(
+                        this, arguments
+                    );
+                    this.handler = new OpenLayers.Handler.Click(
+                        this, {
+                            'click': this.trigger
+                        }, this.handlerOptions
+                    );
+                },
+
+                trigger: function(e) {
+                    var lonlat = map.getLonLatFromPixel(e.xy);
+                    alert("Lat: " + lonlat.lat + " Long: " + lonlat.lon );
+                }
+	});
+	var click = new OpenLayers.Control.Click();
+        map.addControl(click);
+
+        click.activate();
 }
