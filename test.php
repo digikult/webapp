8f35ce9133e969a4e9beef0815685d2273078480
Micke Nordin <mik@elnord.in>
2013-04-11 09:51:18 +0200

    Minor fixes

--- test.php
+++ test.php
@@ -5,6 +5,7 @@ $coord = get_coord();
 <!DOCTYPE html>  
 <html>  
   <head>  
+	<link rel="stylesheet" type="text/css" href="stylesheets/main.css"/>
 	<link rel="stylesheet" type="text/css" href="stylesheets/maps.css"/>
     <script src="js/jquery-1.9.1.min.js"></script>
     <script src="js/geolocation.js"></script>
@@ -22,9 +23,11 @@ $coord = get_coord();
   </head>  
   <body onload="map_init();">  
     <div>  
-      <button id="btnInit" >Find my location via browser/GPS</button><br>  
-      <button onclick="display_coord();">Find my location via IP-address</button>  
+	<image src="images/Corazon.jpg"/><br/>
+      <button id="btnInit" >Find my location via browser/GPS</button><br/><br/>  
+      <button onclick="display_coord();">Find my location via IP-address</button><br/><br/>  
     </div>  
     <div id="basicMap"></div>
+	<p>Heart Image by Ilhh [<a href="http://creativecommons.org/licenses/by-sa/3.0">CC-BY-SA-3.0</a>], <a href="https://commons.wikimedia.org/wiki/File%3ACorazon-.jpg">via Wikimedia Commons</a></p>
   </body>  
 </html> 
