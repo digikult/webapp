7593aa0977c6d853c6086fdf880d85872dbcee15
Micke Nordin <mik@elnord.in>
2013-04-11 11:08:46 +0200

    unused file

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function httpGet(theUrl) {
-	var xmlHttp = null;
-
-	xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", theUrl, false );
-	xmlHttp.send( null );
-	return xmlHttp.responseText;
-}
