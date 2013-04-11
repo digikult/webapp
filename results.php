abde0b570b68dec1ba2c9362dade433d912e924c
Linus Ländin <linus.landin@creativecamp.se>
2013-04-11 10:43:59 +0200

    Added info into skeleton classes

---
+++ results.php
@@ -0,0 +1,11 @@
+<?php
+require_once("functions.php");
+require_once("classes.php");
+/*
+$coords = get_coords_from_GET();
+
+// Initialize classes
+$europe = new europeiana($coords);
+$europe->get_html();
+*/
+?>
